44ddc3695f4382dc5ccc6de5312e299aa5165b9f
ntop <ntop.liu@gmail.com>
2015-12-24 22:40:36 +0800

    rewrite WebcomeSpeech for index.html

--- engine/website/src/flux/index.js
+++ engine/website/src/flux/index.js
@@ -14,7 +14,7 @@ var index = React.createClass({
       <Site>
         <div className="hero">
           <div className="wrap">
-            <div className="text"><strong>Android Flux</strong></div>
+            <div className="text"><strong>AndroidFlux</strong></div>
             <div className="minitext">
               当Android遇到了Flux，架构Android应用的新方式。
             </div>
@@ -23,10 +23,14 @@ var index = React.createClass({
 
         <section className="content wrap">
           <section className="home-section home-getting-started">
-            <p>
+            <p style="line-height:1.7;">
             AndroidFlux是Facebook的<a href="https://facebook.github.io/flux/">Flux</a> 架构的Android实现。
             Flux是Facebook在14年提出的一种Web前端架构，这种架构可以很好的应用于Android平台，相对于其他的MVC/MVP/MVVM等模式，
             拥有良好的文档和更具体的设计，比较适合于快速开发实现。
+            </p>
+            <p style="line-height:1.7;">
+             我们正在做Flux在Android平台的适配工作，有一些文档需要翻译，并且提供Android版本的适配。欢迎
+             <a href="/support.html">加入我们</a>。
             </p>
           </section>
 
